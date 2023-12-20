0a02d9481caef747c01fefe323e6999b4d88e4fc
Michael Celentano <mvcelentano@gmail.com>
2023-12-20 11:53:58 -0500

    fixed lint errors

--- bdms/bdms_utils.py
+++ bdms/bdms_utils.py
@@ -16,15 +16,18 @@ class RandomizedSet:
         if del_name not in self._name_to_idx:
             return False
         # Swap the element with the last element
-        last_name, del_idx = self._idx_to_name[self.size-1], self._name_to_idx[del_name]
+        last_name, del_idx = (
+            self._idx_to_name[self.size - 1],
+            self._name_to_idx[del_name],
+        )
         self._name_to_idx[last_name], self._idx_to_name[del_idx] = del_idx, last_name
         # Remove the last element
         del self._name_to_idx[del_name]
-        del self._idx_to_name[self.size-1]
+        del self._idx_to_name[self.size - 1]
         self.size -= 1
         return True
 
-    def choice(self,rng):
+    def choice(self, rng):
         random_idx = rng.choice(self.size)
         return self._idx_to_name[random_idx]
 
